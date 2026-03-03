5140c233ba143acca692b3d59615c20bb1553c5e
unknown <anishmall3879@gmail.com>
2026-03-03 14:18:17 +0530

    uc1 completed bt [daiwik]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    static void main() {
+        System.out.println("welcome to palindrome checker");
+        System.out.println("version 1.0");
+        System.out.println("system initialised");
+    }
+}
